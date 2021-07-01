3ed496b1c47a7ffced061197fbd9eb899ea68593
liukunhao <954001442@qq.com>
2021-07-01 15:07:32 +0800

    add homePage

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,21 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <div id="app">
+    <HomePage/>
   </div>
-  <router-view/>
 </template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component';
+import HomePage from './views/HomePage.vue'
+
+@Options({
+	name: 'App',
+	components: {
+		HomePage
+	},
+})
+export default class App extends Vue {}
+</script>
 
 <style>
 #app {
@@ -15,16 +26,4 @@
   color: #2c3e50;
 }
 
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
---
+++ src/views/HomePage.vue
@@ -0,0 +1,14 @@
+<template>
+	<h1>Home Page</h1>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component';
+
+@Options({
+	name: 'HomePage',
+	components: {
+	},
+})
+export default class HomePage extends Vue {}
+</script>
