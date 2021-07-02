fd8df8297f005a6bc063c641050d077df3ae574a
Never-Panic <954001442@qq.com>
2021-07-02 15:19:06 +0800

    main screen layout

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,74 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+	<div>
+		<div class="covidMap">
+		<!-- <iframe :src="ruta"></iframe> -->
+		</div>
+		<div class="news">
+			
+		</div>
+		
+		<div class="wraper">
+			<div class="section">
+			</div>
+			
+			<div class="section">
+			</div>
+			
+			<div class="section">
+			</div>
+			
+			<div class="section">
+			</div>
+		</div>
+	</div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
+	name: 'Home',
+    data: () => ({
+        ruta: 'https://covid19.health/'
+    })
 }
 </script>
+
+<style>
+.covidMap {
+	/* outline: #21ff06 dotted thick; */
+	background-color: #2C3E50;
+	
+	width: 100%;
+	height: 1000px;
+	
+	margin-bottom: 10px;
+}
+.news {
+	/* outline: #21ff06 dotted thick; */
+	background-color: antiquewhite;
+	
+	width: 100%;
+	height: 600px;
+	
+	margin-bottom: 10px;
+
+}
+
+.wraper {
+	width: 100%;
+	height: 900px;
+	
+	margin-bottom: 10px;
+	
+	display: flex;
+	flex-wrap: wrap;
+	justify-content: space-around;
+}
+
+.section {
+	/* outline: #21ff06 dotted thick; */
+	background-color: burlywood;
+	
+	width: 49%;
+	margin-bottom: 10px;
+}
+</style>
