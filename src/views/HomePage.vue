379fadf700cf548b7532dff80824956ca40facc4
Yinghao Zhu <zhuyinghao1999@msn.cn>
2021-07-02 09:33:48 +0800

    feat: import HomePage

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,24 +1,21 @@
 <template>
-
-	<n-gradient-text
-	    gradient="linear-gradient(90deg, red 0%, green 50%, blue 100%)"
-		style='margin-top: 20px;'
-	>
-	   <b>为中华民族复兴而写代码</b>
-	</n-gradient-text>
-	
+  <n-gradient-text
+    gradient="linear-gradient(90deg, red 0%, green 50%, blue 100%)"
+    style="margin-top: 20px"
+  >
+    <b>为中华民族复兴而写代码</b>
+  </n-gradient-text>
 </template>
 
 <script>
-import { Options, Vue } from 'vue-class-component';
-import { NGradientText } from 'naive-ui';
+import { Options, Vue } from "vue-class-component";
+import { NGradientText } from "naive-ui";
 
 @Options({
-	name: 'HomePage',
-	components: {
-		NGradientText
-	},
+  name: "HomePage",
+  components: {
+    NGradientText,
+  },
 })
-
 export default class HomePage extends Vue {}
 </script>
