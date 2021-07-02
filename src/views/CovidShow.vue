1c4d111583bf6124b3f8c235a465c2cf29af1d95
Never-Panic <954001442@qq.com>
2021-07-02 14:43:11 +0800

    mainscreen layout

--- src/views/CovidShow.vue
+++ src/views/CovidShow.vue
@@ -1,6 +1,26 @@
 <template>
-    <iframe :src="ruta" width="80%" height=800px></iframe>
+	<div class="covidMap">
+    <!-- <iframe :src="ruta"></iframe> -->
+	</div>
+	<div class="news">
+		
+	</div>
+	
+	<div class="wraper">
+		<div class="section">
+		</div>
+		
+		<div class="section">
+		</div>
+		
+		<div class="section">
+		</div>
+		
+		<div class="section">
+		</div>
+	</div>
 </template>
+
 <script>
 export default {
     data: () => ({
@@ -8,3 +28,44 @@ export default {
     })
 }
 </script>
+
+<style>
+.covidMap {
+	/* outline: #21ff06 dotted thick; */
+	background-color: #2C3E50;
+	
+	width: 100%;
+	height: 1000px;
+	
+	margin-bottom: 10px;
+}
+.news {
+	/* outline: #21ff06 dotted thick; */
+	background-color: antiquewhite;
+	
+	width: 100%;
+	height: 600px;
+	
+	margin-bottom: 10px;
+
+}
+
+.wraper {
+	width: 100%;
+	height: 900px;
+	
+	margin-bottom: 10px;
+	
+	display: flex;
+	flex-wrap: wrap;
+	justify-content: space-around;
+}
+
+.section {
+	/* outline: #21ff06 dotted thick; */
+	background-color: burlywood;
+	
+	width: 49%;
+	margin-bottom: 10px;
+}
+</style>
