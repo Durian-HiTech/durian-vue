f3e4e835bb0367f38e32962e2d0a46fc352c52a9
Never-Panic <954001442@qq.com>
2021-07-03 15:52:09 +0800

    login and register func

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import App from './App.vue'
 import router from './router'
 import {gsap, ScrollTrigger} from 'gsap/all'
 import ElementUI from 'element-ui'
-import Message from 'element-ui'
+
 import "element-ui/lib/theme-chalk/index.css"
 import axios from 'axios'
 
@@ -13,7 +13,6 @@ Vue.use(ElementUI)
 
 Vue.prototype.$gsap = gsap
 Vue.prototype.$axios = axios
-Vue.prototype.$message = Message
 
 gsap.registerPlugin(ScrollTrigger)
 
