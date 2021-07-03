888cc11b33930dad301866fe37e641a9d966d62f
Never-Panic <954001442@qq.com>
2021-07-03 14:49:53 +0800

    login page

--- src/main.js
+++ src/main.js
@@ -2,8 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import {gsap, ScrollTrigger} from 'gsap/all'
+import ElementUI from 'element-ui';
+import "element-ui/lib/theme-chalk/index.css";
 
 Vue.config.productionTip = false
+
+Vue.use(ElementUI)
+
 Vue.prototype.$gsap = gsap
 
 
--- src/router/index.js
+++ src/router/index.js
@@ -16,14 +16,11 @@ const routes = [
     name: 'Vaccine',
     component: Vaccine
   },
-  // {
-  //   path: '/about',
-  //   name: 'About',
-  //   // route level code-splitting
-  //   // this generates a separate chunk (about.[hash].js) for this route
-  //   // which is lazy-loaded when the route is visited.
-  //   component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  // }
+  {
+	path: '/login',
+	name: 'Login',
+	component: () => import('../views/Login.vue')
+  },
 ]
 
 const router = new VueRouter({
