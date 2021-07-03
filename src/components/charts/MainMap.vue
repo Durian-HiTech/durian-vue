bf74b6d225f3c3e901ff4ceb0a8664471e510c11
Coach257 <45021066+Coach257@users.noreply.github.com>
2021-07-03 15:44:33 +0800

    fea: main-map
    world+china

---
+++ src/components/charts/MainMap.vue
@@ -0,0 +1,56 @@
+<template>
+    <div id="main-map" style="width: 600px;height: 500px"></div>
+</template>
+<script>
+import * as echarts from 'echarts';
+export default {
+  name: 'MainMap',
+  mounted(){
+      this.myChart = echarts.init(document.getElementById('main-map'));
+      this.myChart.showLoading();
+      this.loadMap();
+      var _this = this;
+      this.myChart.on('click',function(param){
+          _this.clickevent(param);
+      })
+  },
+  data(){
+      return {
+          myChart:'',
+          mapName:'world',
+          option:''
+      }
+  },
+  watch:{
+      mapName(){
+          this.loadMap();
+      }
+  },
+  methods:{
+      loadMap(){
+          const mapData = require('../../data/map/json/'+this.mapName);
+          echarts.registerMap(this.mapName,mapData);
+          this.option = {
+              series:[
+                  {
+                      name:this.mapName,
+                      type:'map',
+                      roam:'true',
+                      map:this.mapName,
+                      emphasis:{
+                          label:{
+                              show:true
+                          }
+                      }
+                  }
+              ]
+          }
+          this.myChart.setOption(this.option);
+          this.myChart.hideLoading();
+      },
+      clickevent(param){
+          this.mapName = param.name.toLowerCase();
+      }
+  }
+}
+</script>
--- src/main.js
+++ src/main.js
@@ -2,10 +2,8 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import {gsap, ScrollTrigger} from 'gsap/all'
-
 Vue.config.productionTip = false
 Vue.prototype.$gsap = gsap
-
 
 gsap.registerPlugin(ScrollTrigger)
 
