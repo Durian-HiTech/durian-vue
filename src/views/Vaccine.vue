eb5aa93bb709a9b11844c6256753eabf7cf312e6
Yinghao Zhu <zhuyinghao1999@msn.cn>
2021-07-04 22:37:51 +0800

    refactor: move echart dir to charts

--- src/views/Vaccine.vue
+++ src/views/Vaccine.vue
@@ -6,9 +6,9 @@
   </div>
 </template>
 <script>
-// import bargraph from "@/components/echart/BarGraph.vue"
-import linechart from "@/components/echart/LineChart.vue"
-import linechart2 from "@/components/echart/LineChartCmp.vue"
+// import bargraph from "@/components/charts/BarGraph.vue"
+import linechart from "@/components/charts/LineChart.vue"
+import linechart2 from "@/components/charts/LineChartCmp.vue"
 export default {
   name: "VaccineBarGraph",
   components: {
