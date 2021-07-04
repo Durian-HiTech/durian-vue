ee0d9d8585290da838a0729a6f0bbb5bddf39704
Never-Panic <954001442@qq.com>
2021-07-04 11:22:41 +0800

    change storage format

--- src/main.js
+++ src/main.js
@@ -5,44 +5,19 @@ import {gsap, ScrollTrigger} from 'gsap/all'
 import ElementUI from 'element-ui'
 import "element-ui/lib/theme-chalk/index.css"
 import axios from 'axios'
-import Vuex from 'vuex'
-import createPersistedState from 'vuex-persistedstate'
+import store from './storage/store.js'
 
 Vue.config.productionTip = false
 
 Vue.use(ElementUI)
-Vue.use(Vuex)
 
 Vue.prototype.$gsap = gsap
 Vue.prototype.$axios = axios
 
 gsap.registerPlugin(ScrollTrigger)
 
-
-// storage
-const store = new Vuex.Store({
-  state: {
-    count: 0
-  },
-  mutations: {
-    increment (state) {
-      state.count++
-    },
-	reset (state) {
-		state.count = 0
-	}
-  },
-  getters: {
-	count: state => {
-		return 'The count is:'+state.count
-	}
-  },
-  plugins: [createPersistedState()]
-})
-
-
 new Vue({
   router,
   render: h => h(App),
-  store: store,
+  store: store.store,
 }).$mount('#app')
