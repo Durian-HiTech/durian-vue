9e3c06a977835cdb0681ac304577d28af7f8b06d
ztx <1213587739@qq.com>
2021-07-04 15:31:35 +0800

    Update NewsList.vue

--- src/components/NewsList.vue
+++ src/components/NewsList.vue
@@ -1,11 +1,11 @@
 <template>
     <div>
             <div
-            style="font-size:12px"
+            style="font-size:14px"
             v-for="post in posts"
-            v-bind:key="post.id"
-            v-bind:title="post.title"
-        ><router-link :to="'news/'+post.id">{{post.title|ellipsis}}</router-link> <el-divider></el-divider></div>
+            v-bind:key="post.news_id"
+            v-bind:title="post.news_title"
+        ><router-link :to="'news/'+post.id">{{post.news_title|ellipsis}}</router-link> <el-divider></el-divider></div>
     </div>
     
     
@@ -13,14 +13,15 @@
 
 
 <script>
-
+import api from '../commonApi.js'
 export default {
     // name:"NewsList",
     data(){
         return {
             posts: [
-                 {id:"1",title: '关于印发新型冠状病毒肺炎诊疗方案（试行第八版 修订版）的通知',link:'http://www.gov.cn/zhengce/zhengceku/2021-04/15/content_5599795.htm'} ,
-                 {id:"2",title: '关于调整《新冠肺炎疫情相关租金减让会计处理规定》适用范围的通知',link:'http://www.gov.cn/zhengce/zhengceku/2021-06/04/content_5615523.htm'} ,
+            //      {news_id:"1",news_title: '关于印发新型冠状病毒肺炎诊疗方案（试行第八版 修订版）的通知',news_content: "新闻内容01",news_created_time: "2021-07-04T10:53:50Z"} ,
+            //      {news_id:"2",news_title: '关于调整《新冠肺炎疫情相关租金减让会计处理规定》适用范围的通知',news_content: "新闻内容01",news_created_time: "2021-07-04T10:53:50Z"} ,
+            // 
             ]
         }
     },
@@ -28,16 +29,44 @@ export default {
     ellipsis (value) {
       if (!value) return ''
       if (value.length > 25) {
-        return value.slice(0,25) + '...'//0:下标,从第一个字开始显示,50:显示字数,多余用...代替
+        return value.slice(0,20) + '...'//0:下标,从第一个字开始显示,50:显示字数,多余用...代替
       }
       return value
+    },
+    },
+    
+    methods:{
+    },
+    
+    mounted : function(){
+        var _this = this
+         this.$axios
+			.get(api.baseApi+'/news/list_all_news')
+                .then(function(response) {
+                    console.log(response.data)
+                    if (response.data.success) {  
+                        for ( var m in response.data.data){
+                            _this.posts.push(response.data.data[m])
+                            console.log(response.data.data)
+                        }
+
+                        
+                        console.log(_this.posts)
+				}else {
+					this.$message({message: response.data.message,
+									type: 'error'})
+				}
+                })
     }
-  }
+        
+    ,
+    }
+  
     // GoUrl(){
     //     window.open(this.UrlAddr,"_blank"); //在新窗口打开外链接
     //     window.location.href = this.UrlAddr; //在本页面打开外部链接
     // }
-}
+
 </script>
 
 <style>
