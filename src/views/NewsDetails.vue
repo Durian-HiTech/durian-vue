b1234097c3e01d3847f058f0262a1717a2c1342c
hahahao11 <63183726+hahahao11@users.noreply.github.com>
2021-07-04 16:33:41 +0800

    Update NewsDetails.vue

--- src/views/NewsDetails.vue
+++ src/views/NewsDetails.vue
@@ -4,41 +4,29 @@
 <el-container>
   <el-header>导航栏</el-header>
   <el-main >
-<el-row :gutter="10">
-  <el-col >
-    <div >
-      <el-card class="box-card test" shadow="hover">
-        <div slot="header" class="clearfix">
-          <span style ="font-size:18px;font-weight: 700;">中国病例最新变化情况</span>
-         </div>
-          <div class="text item"  >这是新闻1</div>
-             <div class="text item"  >这是新闻2</div>
-             <div class="text item"  >新闻3</div>
-          
-            
-      </el-card>
-    </div>
-  </el-col>
-
-
-  
-</el-row>
-
-
-</el-main>
+    <el-row :gutter="10">
+      <el-col >
+        <div >
+          <el-card class="box-card test" shadow="hover">
+            <div slot="header" class="clearfix">
+              <span style ="font-size:18px;font-weight: 700;">title</span>
+            </div>
+            <div class="text item"  >content</div>
+          </el-card>
+        </div>
+      </el-col>
+    </el-row>
+  </el-main>
 </el-container>
   </div>
 </template>
+
 <script>
-//import NewsList from '@/components/NewsList.vue';
-// import bargraph from "@/components/echart/bar-graph.vue"
-//import linechart from "@/components/echart/line_chart.vue"
+
 export default {
   name: "NewsDetails",
   components: {
-    //NewsList
-    // bargraph,
-    //linechart
+
   },
   data() {
     return {
@@ -48,55 +36,5 @@ export default {
 }
 </script>
 <style>
-  .text {
-    
-    font-size: 14px;
-  }
 
-  .item {
-    
-    padding: 30px 0;
-  }
-
-  .box-card {
-    
-    align-items: center;
-    border-radius: 4px;
-    
-  }
-  .test{
-    
-    align-items: center;
-    width: 1500px;
-    height: 700px;
-    background: rgb(250, 222, 213);
-    transition: all 0.4s;
-    -moz-transition:all .4s;
-    -webkit-transition:all .4s;
-    -o-transition:all .4s;
-    background: url(../assets/backgroundImage/news1.jpg);
-    background-repeat:no-repeat;   /* 不会重复 */
-    background-position: center;   /* 是为了让图片放大的时候从中部放大，不会偏移 */
-    background-size: 100% 100%;
-  }
-  .test:hover{
-    background-size: 110% 110%;
-  }
-   
-  .el-row {
-    margin-bottom: 20px;
-   
-  }
-  .el-col {
-    border-radius: 4px;
-  }
-  .clearfix:before,
-  .clearfix:after {
-    display: table;
-    content: "";
-    
-  }
-  .clearfix:after {
-    clear: both
-  }
 </style>
