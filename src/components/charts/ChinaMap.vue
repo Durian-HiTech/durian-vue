1d6963fb23648a8b7f1f9956c8f326b42e7ac603
qlylangyu <1252536415@qq.com>
2021-07-06 03:14:56 +0800

    fix ChinaMap

--- src/components/charts/ChinaMap.vue
+++ src/components/charts/ChinaMap.vue
@@ -3,9 +3,22 @@
 </template>
 <script>
 import * as echarts from "echarts";
+import risk_areas from "../../data/map/json/list_all_high_risk_areas.json";
+// import $ from "jquery";
+
 var data = [];
 export default {
   name: "ChinaMap",
+  props: {
+    medium_risk: {
+      type: Array,
+      required: true,
+    },
+    high_risk: {
+      type: Array,
+      required: true,
+    }
+  },
   mounted() {
     this.myChart = echarts.init(document.getElementById("china-map"));
     this.myChart.showLoading();
@@ -36,17 +49,16 @@ export default {
               });
                       // + '<br/>';
               // res += params.seriesName;
-              return res[0].name;
+              return res[0].name + '<br/>' + params.value;
             }
 
         },
         visualMap: {
             left: 'right',
             pieces: [
-                {min: 0,max:999,label:'小于1000',color:'#ffffff'},
-                {min: 1000, max: 10000,label:'1000-10000',color:'#fee090'},
-                {min: 10000, max: 100000,label:'10000-100000',color:'#f46d43'},
-                {min: 100000,label:'大于100000',color:'#a50026'},
+                {min: 0, max: 0,label:'低风险',color:'#fee090'},
+                {min: 1, max: 1,label:'中风险',color:'#f46d43'},
+                {min: 2, max: 2,label:'高风险',color:'#a50026'},
             ],
         },
         series: [
@@ -71,6 +83,7 @@ export default {
                   show: false,
                 }
               },
+            data: [],
             // itemStyle: {
             //   normal: {
             //     label: {
@@ -108,11 +121,28 @@ export default {
       this.option["series"][0].normal.label.show = false;
       this.option["series"][0].emphasis.label.show = false;
       console.log(mapData.features);
+      console.log(risk_areas);
       var features = mapData.features;
       features.forEach((item)=>{
         // console.log(item.properties);
-        data.push(item.properties);
+        var properties = item.properties;
+        data.push(properties);
+        var newdata = {name: properties.adcode, value: 0};
+        this.$props.medium_risk.forEach((item1)=>{
+          if(properties.name === item1.province || properties.name === item1.city){
+            newdata.value = 1;
+          }
+        });
+        this.$props.high_risk.forEach((item1)=>{
+          if(properties.name === item1.province || properties.name === item1.city){
+            newdata.value = 2;
+          }
+        });
+        console.log(newdata);
+        this.option["series"][0].data.push(newdata);
       });
+      console.log(this.$props.medium_risk);
+      console.log(this.option["series"][0].data);
       console.log(data);
       this.myChart.setOption(this.option);
       this.myChart.hideLoading();
