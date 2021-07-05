ebcaec69acc3b38094712c668d1ecd293f6aaa17
Coach257 <45021066+Coach257@users.noreply.github.com>
2021-07-05 17:56:13 +0800

    feat: 地图api 对接

--- src/components/charts/MainMap.vue
+++ src/components/charts/MainMap.vue
@@ -3,35 +3,36 @@
 </template>
 <script>
 import * as echarts from "echarts";
+import api from "../../commonApi.js";
+import $ from "jquery";
 export default {
   name: "MainMap",
-  props:{
-    country:{
-      type:String,
-      required:true,
-      default:'world'
+  props: {
+    country: {
+      type: String,
+      required: true,
+      default: "world",
     },
-    type:{
-      type:String,
-      required:true,
-      default:'cases'
+    type: {
+      type: String,
+      required: true,
+      default: "cases",
     },
-    data:{
-      type:Array,
-      required:true
-    }
+    data: {
+      type: Array,
+      required: true,
+    },
   },
   mounted() {
     this.myChart = echarts.init(document.getElementById("main-map"));
     this.myChart.showLoading();
+    var _this = this;
     this.myChart.on("click", function (param) {
       _this.clickevent(param);
     });
     this.loadMap();
     this.loadData();
     this.loadType();
-    var _this = this;
-
   },
   data() {
     return {
@@ -40,11 +41,11 @@ export default {
         title: {
           text: "新型冠状病毒肺炎疫情图",
           textStyle: {
-            color: "#fff", 
+            color: "#fff",
           },
           subtext: "数据来源于官方",
           subtextStyle: {
-            color: "#fff", 
+            color: "#fff",
           },
           left: "center",
         },
@@ -52,14 +53,15 @@ export default {
           trigger: "item",
           showDelay: 0,
           transitionDuration: 0.2,
-          formatter: function (params) {// 光标浮动显示内容控制
+          formatter: function (params) {
+            // 光标浮动显示内容控制
             var mapping = {
               cases: "确诊人数",
               deaths: "死亡人数",
               recovered: "治愈人数",
               vaccine: "疫苗接种人数",
             };
-            var res = params.name + '<br/>';
+            var res = params.name + "<br/>";
             res += mapping[params.seriesName] + ":" + params.value;
             return res;
           },
@@ -67,12 +69,17 @@ export default {
         visualMap: {
           left: "right",
           textStyle: {
-            color: "#fff", 
+            color: "#fff",
           },
           pieces: [
             { min: 0, max: 999, label: "小于1000", color: "#FFFACD" },
             { min: 1000, max: 10000, label: "1000-10000", color: "#fee090" },
-            { min: 10000, max: 100000, label: "10000-100000", color: "#f46d43"},
+            {
+              min: 10000,
+              max: 100000,
+              label: "10000-100000",
+              color: "#f46d43",
+            },
             { min: 100000, label: "大于100000", color: "#a50026" },
           ],
         },
@@ -87,7 +94,7 @@ export default {
                 show: true,
               },
             },
-            data:[],
+            data: [],
           },
         ],
       },
@@ -97,39 +104,55 @@ export default {
     country() {
       this.loadMap();
     },
-    type(){
+    type() {
       this.loadType();
     },
-    data(){
+    data() {
       this.loadData();
-    }
+    },
   },
   methods: {
     loadMap() {
-      const mapData = require("../../data/map/json/" + this.$props.country);
+      let formData = new FormData();
+      formData.append("name", this.$props.country);
+      var mapData;
+      $.ajax({
+        url:api.baseApi + "/data/query_data",
+        type:'POST',
+        async:false,
+        data:{
+          name:this.$props.country
+        },
+        dataType:'json',
+        success:function(response){
+          console.log(response);
+          mapData = response.data;
+        }
+      })
       echarts.registerMap(this.$props.country, mapData);
       this.option["series"][0]["map"] = this.$props.country;
-      this.option["series"][0]["center"] = ['50%, 50%'];
-      this.option["series"][0]["zoom"] = 1
+      this.option["series"][0]["center"] = ["50%, 50%"];
+      this.option["series"][0]["zoom"] = 1;
       this.option["series"][0]["center"] = undefined;
       this.myChart.setOption(this.option);
       this.myChart.hideLoading();
     },
-    clickevent(param) {//地图点击事件函数
+    clickevent(param) {
+      //地图点击事件函数
       this.changemap(param.name);
     },
-    changemap(name) {//地图改变事件函数
+    changemap(name) {
+      //地图改变事件函数
       this.$parent.changeCountry(name);
     },
-    loadData(){
+    loadData() {
       this.option["series"][0]["data"] = this.$props.data;
       this.myChart.setOption(this.option);
     },
-    loadType(){
+    loadType() {
       this.option["series"][0]["name"] = this.$props.type;
       this.myChart.setOption(this.option);
-    }
-
+    },
   },
 };
 </script>
