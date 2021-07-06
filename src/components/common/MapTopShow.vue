dd2fb28794825ec180ca8f96c900261ddb05379e
Coach257 <45021066+Coach257@users.noreply.github.com>
2021-07-06 15:19:06 +0800

    feat: maptopshow框架 :-1:

---
+++ src/components/common/MapTopShow.vue
@@ -0,0 +1,12 @@
+<template>
+    <map-top-show-card></map-top-show-card>
+</template>
+<script>
+import MapTopShowCard from "./MapTopShowCard.vue"
+export default ({
+    name:"MapTopShow",
+    components:{
+        MapTopShowCard
+    },
+})
+</script>
