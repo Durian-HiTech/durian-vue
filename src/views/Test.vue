a954fa42554683f0facbb6deb02ce243f4f60c0f
Never-Panic <954001442@qq.com>
2021-07-07 09:22:28 +0800

    fix: risk map style modify

--- src/views/Test.vue
+++ src/views/Test.vue
@@ -5,16 +5,16 @@
 		<h2 style="margin-top: 20px;"> user state</h2>
 		<el-button @click="login()"> login </el-button>
 		<el-button @click="reset()"> reset </el-button>
-		<RiskMap/>
+		<CovidMap/>
 	</div>
 </template>
 
 <script>
-import RiskMap from "../components/RiskMap";
+import CovidMap from "../components/CovidMap";
 export default {
 	name: 'Test',
 	components: {
-		RiskMap
+		CovidMap
 	},
 	methods: {
 		reset () {
