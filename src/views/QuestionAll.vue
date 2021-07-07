b355c8499d183061b89d97e2310d17e4c5bdceec
Xi Lan <48905285+IAmParasite@users.noreply.github.com>
2021-07-07 13:44:47 +0800

    feat: 添加了提问功能

--- src/views/QuestionAll.vue
+++ src/views/QuestionAll.vue
@@ -3,6 +3,31 @@
     <span class="title">
       <b>Covid</b> Q&A
     </span>
+    <el-button type="text" @click="dialogVisible = true">添加问题</el-button>
+
+      <el-dialog
+          title="提示"
+          :visible.sync="dialogVisible"
+          width="30%"
+          :before-close="handleClose">
+        <span>这是一段信息</span>
+        <el-form :model="ruleForm" :rules="rules" ref="ruleForm" label-width="100px" class="demo-ruleForm">
+          <el-form-item label="问题名称" prop="name">
+            <el-input v-model="ruleForm.name"></el-input>
+          </el-form-item>
+          <el-form-item label="问题内容" prop="name">
+            <el-input v-model="ruleForm.content"></el-input>
+          </el-form-item>
+
+
+          <el-form-item>
+            <el-button type="primary" @click="submitForm('ruleForm')">立即创建</el-button>
+            <el-button @click="resetForm('ruleForm')">重置</el-button>
+          </el-form-item>
+        </el-form>
+        <span slot="footer" class="dialog-footer">
+    </span>
+    </el-dialog>
     <el-divider/>
     <QuestionList/>
   </div>
@@ -11,11 +36,80 @@
 
 <script>
 import QuestionList from '../components/QuestionList'
+import axios from "axios";
 
 export default {
   name: 'QuestionAll',
   components: {
     QuestionList,
+  },
+  data() {
+    return {
+      dialogVisible: false,
+      ruleForm: {
+        name: '',
+        content: '',
+      },
+      rules: {
+        name: [
+          { required: true, message: '请输入问题标题', trigger: 'blur' },
+          { min: 1, max: 10, message: '长度在 1 到 10 个字符', trigger: 'blur' }
+        ],
+        content: [
+          { required: true, message: '请输入问题内容', trigger: 'blur' },
+          { min: 1, max: 100, message: '长度在 1 到 100 个字符', trigger: 'blur' }
+        ],
+      }
+    }
+  },
+  methods: {
+    handleClose(done) {
+      this.$confirm('确认关闭？')
+          // eslint-disable-next-line no-unused-vars
+          .then(_ => {
+            done();
+          })
+          // eslint-disable-next-line no-unused-vars
+          .catch(_ => {});
+    },
+    submitForm(formName) {
+      this.$refs[formName].validate((valid) => {
+        if (valid) {
+          this.dialogVisible = false
+          console.log(this.ruleForm)
+          let formData = new FormData();
+          formData.append("user_id", this.$store.getters.userState.id);
+          formData.append("question_title", this.ruleForm.name);
+          formData.append("question_content", this.ruleForm.content);
+          let config = {
+            headers: {"Content-Type": "multipart/form-data",},
+          };
+          // var _this = this;
+          axios.post(
+              "https://durian-go-318509.df.r.appspot.com/api/v1/notice/create_question",
+              formData,
+              config
+          ).then(function (response) {
+                console.log(response.status)
+                if (response.status == 200) {
+                  console.log((response))
+
+                } else {
+                  console.log("请求失败");
+                  // console.log(response.data);
+                  // _this.fail()
+                }
+              });
+        } else {
+          console.log('error submit!!');
+          return false;
+        }
+      });
+    },
+    resetForm(formName) {
+      this.$refs[formName].resetFields();
+    }
+
   }
 }
 </script>
@@ -30,8 +124,8 @@ export default {
   }
 
 .title {
-    font-size:80px; 
-    align-self: flex-start; 
+    font-size:80px;
+    align-self: flex-start;
     position: relative;
     left: 250px;
   }
