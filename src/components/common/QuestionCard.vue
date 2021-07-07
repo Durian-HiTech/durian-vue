402f074c27fa3574971024147aff1c9cdee902f0
Never-Panic <954001442@qq.com>
2021-07-07 13:15:59 +0800

    fix: question re format

---
+++ src/components/common/QuestionCard.vue
@@ -0,0 +1,105 @@
+<template>
+  <v-card
+    class="mx-auto"
+    width="460"
+  >
+    <v-img
+      src='../../assets/backgroundImage/question0.jpg'
+      height="200px"
+      v-if="showImage.a"
+    ></v-img>
+
+    <v-img
+      src='../../assets/backgroundImage/question1.jpg'
+      height="200px"
+      v-if="showImage.b"
+    ></v-img>
+
+    <v-img
+      src='../../assets/backgroundImage/question2.jpg'
+      height="200px"
+      v-if="showImage.c"
+    ></v-img>
+
+    <v-card-title>
+      {{title | cutLongTitle}}
+    </v-card-title>
+
+    <v-card-subtitle style="display: flex">
+      Latest Covid Q&A
+    </v-card-subtitle>
+
+    <v-card-actions>
+      <v-btn
+        :color="this.color"
+        text
+        @click="goToNewsPage()"
+      >
+        Explore
+      </v-btn>
+
+      <v-spacer></v-spacer>
+
+      <v-btn
+        icon
+        @click="show = !show"
+      >
+        <v-icon>{{ show ? 'mdi-chevron-up' : 'mdi-chevron-down' }}</v-icon>
+      </v-btn>
+    </v-card-actions>
+
+    <v-expand-transition>
+      <div v-show="show">
+        <v-divider></v-divider>
+
+        <v-card-text>
+          {{content | cutLongText}}
+        </v-card-text>
+      </div>
+    </v-expand-transition>
+  </v-card>
+</template>
+
+
+<script>
+export default {
+    name:'QuestionCard',
+    data () {
+        return {
+            show: false,
+            color: "cyan",
+        }
+    },
+    computed: {
+      showImage () {
+        return {
+          a: (this.title.length % 5) == 0,
+          b: (this.title.length % 5) == 1,
+          c: (this.title.length % 5) == 2
+        }
+      },
+    },
+    props: ['title', 'link', 'content'],
+    methods: {
+      goToNewsPage() {
+        this.$router.push(this.link)
+      },
+    },
+    filters: {
+      cutLongText: function (value) {
+        let maxLenth = 200
+        if (value.length > maxLenth) {
+          return value.slice(0, maxLenth) + '...'
+        }
+        return value
+      },
+      cutLongTitle: function (value) {
+        let maxLenth = 50
+        if (value.length > maxLenth) {
+          return value.slice(0, maxLenth) + '...'
+        }
+        return value
+      }
+    },
+}
+</script>
