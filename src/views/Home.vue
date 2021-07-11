20ca5ca7f91161440217074d5e5aff2c8a262015
Xi Lan <48905285+IAmParasite@users.noreply.github.com>
2021-07-11 16:28:46 +0800

    fix: 尝试显示高风险，但是失败了 :-1:

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -236,7 +236,6 @@ export default {
       this.ChinamapData = homeData["China"]["detailed"];
       this.GlobalmapData = homeData["Global"]["detailed"];
       this.dataLoaded = true;
-      console.log(this.GlobalmapData);
     },
     changeKey(nowtype) {
       var mapping = {
