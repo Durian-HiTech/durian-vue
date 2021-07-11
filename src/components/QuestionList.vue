25261cba885cafa21c3b565ef8d7744a1e81f023
Yinghao Zhu <zhuyinghao1999@msn.cn>
2021-07-11 22:28:18 +0800

    feat: show recommend questions in all question page

--- src/components/QuestionList.vue
+++ src/components/QuestionList.vue
@@ -1,36 +1,41 @@
 <template>
-
   <div class="main">
     <el-dialog
-          title="提示"
-          :visible.sync="dialogVisible"
-          width="30%"
-          :before-close="handleClose">
-        <el-form :model="ruleForm" :rules="rules" ref="ruleForm" label-width="100px" class="demo-ruleForm">
-          <el-form-item label="问题名称" prop="name">
-            <el-input v-model="ruleForm.name"></el-input>
-          </el-form-item>
-          <el-form-item label="问题内容" prop="content">
-            <el-input v-model="ruleForm.content" type="textarea" ></el-input>
-          </el-form-item>
+      title="提示"
+      :visible.sync="dialogVisible"
+      width="30%"
+      :before-close="handleClose"
+    >
+      <el-form
+        :model="ruleForm"
+        :rules="rules"
+        ref="ruleForm"
+        label-width="100px"
+        class="demo-ruleForm"
+      >
+        <el-form-item label="问题名称" prop="name">
+          <el-input v-model="ruleForm.name"></el-input>
+        </el-form-item>
+        <el-form-item label="问题内容" prop="content">
+          <el-input v-model="ruleForm.content" type="textarea"></el-input>
+        </el-form-item>
 
-
-          <el-form-item>
-            <el-button type="primary" @click="submitForm('ruleForm')">立即创建</el-button>
-            <el-button @click="resetForm('ruleForm')">重置</el-button>
-          </el-form-item>
-        </el-form>
-        <span slot="footer" class="dialog-footer">
-    </span>
+        <el-form-item>
+          <el-button type="primary" @click="submitForm('ruleForm')"
+            >立即创建</el-button
+          >
+          <el-button @click="resetForm('ruleForm')">重置</el-button>
+        </el-form-item>
+      </el-form>
+      <span slot="footer" class="dialog-footer"> </span>
     </el-dialog>
 
-    <div style=' display: flex; 
-      justify-content: center; 
-      align-items: flex-start; '>
-
+    <div
+      style="display: flex; justify-content: center; align-items: flex-start"
+    >
       <v-app style="margin: 30px">
         <h1>问答专区</h1>
-        <p style="color:grey;">共{{this.question_list_show.length}}条提问</p>
+        <p style="color: grey">共{{ this.question_list_show.length }}条提问</p>
 
         <center>
           <v-btn
@@ -38,11 +43,9 @@
             color="cyan"
             dark
             @click="dialogVisible = true"
-            width='140px'
+            width="140px"
           >
-            <v-icon left>
-              mdi-pencil
-            </v-icon>
+            <v-icon left> mdi-pencil </v-icon>
             我要提问
           </v-btn>
         </center>
@@ -51,169 +54,215 @@
           <el-input
             placeholder="Search"
             v-model="search"
-            style="width: 350px; margin-top: 20px;"
-            >
+            style="width: 350px; margin-top: 20px"
+          >
             <i slot="prefix" class="el-input__icon el-icon-search"></i>
           </el-input>
         </center>
 
-        <div class='listSection'>
+        <div class="listSection">
           <div
-              style="margin: 30px; "
-              v-for="question in question_list_show.slice((this.currentPage - 1) * this.eachPage,
-                this.currentPage * this.eachPage)"
-              v-bind:key="question.question_id"
+            style="margin: 30px"
+            v-for="question in question_list_show.slice(
+              (this.currentPage - 1) * this.eachPage,
+              this.currentPage * this.eachPage
+            )"
+            v-bind:key="question.question_id"
           >
-            <QuestionCard v-bind:title="question.question_title" :link="'question/'+question.question_id" :content="question.question_content"/>
+            <QuestionCard
+              v-bind:title="question.question_title"
+              :link="'question/' + question.question_id"
+              :content="question.question_content"
+            />
           </div>
         </div>
 
+        <!-- <div class="related_question_header" style="margin-top: 10px; margin-left: 10px; font-size: 18px; text-align: left; ">
+            为您推荐问题
+            <el-divider></el-divider>
+          </div>
+          <div class="related_question_content" style="text-align: left; margin-left: 5px; color: gray;">
+            <div v-for="item in related_list" :key="item.id" style="margin: 10px">
+              <router-link :to="{path: '/question/'+ item.id}" @click.native="refresh">{{ item.content }} </router-link>
+            </div>
+          </div> -->
+
         <v-pagination
-            style="margin-top: 30px;"
-            v-model="currentPage"
-            :length="Math.ceil(this.question_list_show.length / eachPage)"
-            circle
-            color="cyan"
+          style="margin-top: 30px"
+          v-model="currentPage"
+          :length="Math.ceil(this.question_list_show.length / eachPage)"
+          circle
+          color="cyan"
         ></v-pagination>
-
       </v-app>
-
     </div>
-
+    <template>
+      <v-simple-table>
+        <template v-slot:default>
+          <thead>
+            <tr>
+              <th class="text-left">为您推荐问题</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr
+              v-for="item in recommend_questions"
+              :key="item.question_id"
+              @click="goToNewsPage('/question/' + item.question_id)"
+            >
+              <td>{{ item.question_title }}</td>
+            </tr>
+          </tbody>
+        </template>
+      </v-simple-table>
+    </template>
   </div>
 </template>
 
 
 <script>
-import api from '../commonApi.js'
-import QuestionCard from './common/QuestionCard.vue'
-import moment from "moment"
+import api from "../commonApi.js";
+import QuestionCard from "./common/QuestionCard.vue";
+import moment from "moment";
 
 export default {
-    name:"QuestionList",
-    data(){
-        return {
-            question_list: [],
-            currentPage: 1,
-            eachPage: 10,
-            dialogVisible: false,
-            search: '',
-            ruleForm: {
-              name: '',
-              content: '',
-            },
-            rules: {
-              name: [
-                { required: true, message: '请输入问题标题', trigger: 'blur' },
-                { min: 1, max: 10, message: '长度在 1 到 10 个字符', trigger: 'blur' }
-              ],
-              content: [
-                { required: true, message: '请输入问题内容', trigger: 'blur' },
-                { min: 1, max: 100, message: '长度在 1 到 100 个字符', trigger: 'blur' }
-              ],
-            } 
-        }
-    },
-    props: ['title'],
-    components: {
-        QuestionCard,
-    },
-    computed: {
-      question_list_show () {
-        return this.question_list.filter(
-          item => item.question_title.indexOf(this.search)>=0
-        )
+  name: "QuestionList",
+  data() {
+    return {
+      recommend_questions: [],
+      question_list: [],
+      currentPage: 1,
+      eachPage: 10,
+      dialogVisible: false,
+      search: "",
+      ruleForm: {
+        name: "",
+        content: "",
       },
+      rules: {
+        name: [
+          { required: true, message: "请输入问题标题", trigger: "blur" },
+          {
+            min: 1,
+            max: 10,
+            message: "长度在 1 到 10 个字符",
+            trigger: "blur",
+          },
+        ],
+        content: [
+          { required: true, message: "请输入问题内容", trigger: "blur" },
+          {
+            min: 1,
+            max: 100,
+            message: "长度在 1 到 100 个字符",
+            trigger: "blur",
+          },
+        ],
+      },
+    };
+  },
+  props: ["title"],
+  components: {
+    QuestionCard,
+  },
+  computed: {
+    question_list_show() {
+      return this.question_list.filter(
+        (item) => item.question_title.indexOf(this.search) >= 0
+      );
     },
-    methods:{
-        sleep(ms) {
-          return new Promise(resolve => 
-              setTimeout(resolve, ms)
-          )
-        },
-        getAllQuestions() {
-        let formData = new FormData();
-        let config = {
-            headers: {"Content-Type": "multipart/form-data",},
-        };
-        var _this = this;
-        this.$axios.get(
-            api.baseApi+"/notice/list_all_questions",
-            formData,
-            config
-        ).then(function (response) {
-                console.log(response.status)
-                if (response.status == 200) {
-                // console.log((response))
-                _this.question_list = response.data.data
-                for (var i = 0; i < _this.question_list.length; i++) {
-                    _this.question_list[i].question_time = moment(_this.question_list[i].question_time).startOf('day').fromNow();
-                }
-                } else {
+  },
+  methods: {
+    goToNewsPage(link) {
+      this.$router.push(link);
+    },
+    sleep(ms) {
+      return new Promise((resolve) => setTimeout(resolve, ms));
+    },
+    getAllQuestions() {
+      let formData = new FormData();
+      let config = {
+        headers: { "Content-Type": "multipart/form-data" },
+      };
+      var _this = this;
+      this.$axios
+        .get(api.baseApi + "/notice/list_all_questions", formData, config)
+        .then(function (response) {
+          console.log(response.status);
+          if (response.status == 200) {
+            // console.log((response))
+            _this.question_list = response.data.data;
+            _this.recommend_questions = response.data.recommend_questions;
+            for (var i = 0; i < _this.question_list.length; i++) {
+              _this.question_list[i].question_time = moment(
+                _this.question_list[i].question_time
+              )
+                .startOf("day")
+                .fromNow();
+            }
+          } else {
+            console.log("请求失败");
+            // console.log(response.data);
+            // _this.fail()
+          }
+        });
+    },
+    handleClose(done) {
+      this.$confirm("确认关闭？")
+        // eslint-disable-next-line no-unused-vars
+        .then((_) => {
+          done();
+        })
+        // eslint-disable-next-line no-unused-vars
+        .catch((_) => {});
+    },
+    submitForm(formName) {
+      this.$refs[formName].validate((valid) => {
+        if (valid) {
+          this.dialogVisible = false;
+          console.log(this.ruleForm);
+          let formData = new FormData();
+          formData.append("user_id", this.$store.getters.userState.id);
+          formData.append("question_title", this.ruleForm.name);
+          formData.append("question_content", this.ruleForm.content);
+          let config = {
+            headers: { "Content-Type": "multipart/form-data" },
+          };
+          var _this = this;
+          this.$axios
+            .post(api.baseApi + "/notice/create_question", formData, config)
+            .then(function (response) {
+              console.log(response.status);
+              if (response.status == 200) {
+                console.log(response);
+                _this.$message({ message: "提问成功", type: "success" });
+                _this.sleep(10).then(() => {
+                  _this.$router.go(0);
+                });
+              } else {
                 console.log("请求失败");
                 // console.log(response.data);
                 // _this.fail()
-                }
+              }
             });
-        },
-        handleClose(done) {
-          this.$confirm('确认关闭？')
-              // eslint-disable-next-line no-unused-vars
-              .then(_ => {
-                done();
-              })
-              // eslint-disable-next-line no-unused-vars
-              .catch(_ => {});
-        },
-        submitForm(formName) {
-          this.$refs[formName].validate((valid) => {
-            if (valid) {
-              this.dialogVisible = false
-              console.log(this.ruleForm)
-              let formData = new FormData();
-              formData.append("user_id", this.$store.getters.userState.id);
-              formData.append("question_title", this.ruleForm.name);
-              formData.append("question_content", this.ruleForm.content);
-              let config = {
-                headers: {"Content-Type": "multipart/form-data",},
-              };
-               var _this = this;
-              this.$axios.post(
-                  api.baseApi+"/notice/create_question",
-                  formData,
-                  config
-              ).then(function (response) {
-                    console.log(response.status)
-                    if (response.status == 200) {
-                      console.log((response))
-                       _this.$message({ message: "提问成功", type: "success" });
-                      _this.sleep(10).then(()=>{
-                        _this.$router.go(0)
-                      })
-                    } else {
-                      console.log("请求失败");
-                      // console.log(response.data);
-                      // _this.fail()
-                    }
-                  });
-            } else {
-              console.log('error submit!!');
-              return false;
-            }
-          });
-        },
-        resetForm(formName) {
-          this.$refs[formName].resetFields();
-        },
-      },
-    mounted : function(){
-        this.getAllQuestions();
+        } else {
+          console.log("error submit!!");
+          return false;
+        }
+      });
     },
-}
+    resetForm(formName) {
+      this.$refs[formName].resetFields();
+    },
+  },
+  mounted: function () {
+    this.getAllQuestions();
+  },
+};
 </script>
 
 <style scoped>
-.main{
+.main {
   /*background-color: violet;*/
   display: flex;
   flex-direction: column;
@@ -222,8 +271,8 @@ export default {
 .listSection {
   /* outline: #00ff00 dotted thick; */
 
-  display: flex; 
-  justify-content: center; 
+  display: flex;
+  justify-content: center;
   align-items: center;
   flex-wrap: wrap;
 
