ce5b7d32d3a4334d6681066ffbc8d0a31261e37f
Xi Lan <48905285+IAmParasite@users.noreply.github.com>
2021-07-13 03:01:38 +0800

    fix

--- src/views/GlobalAnalysisTabDetailedCmp.vue
+++ src/views/GlobalAnalysisTabDetailedCmp.vue
@@ -117,7 +117,7 @@ export default {
         },
       ],
       type: "nowcases",
-      countries: ['China', 'United States of America', 'Japan'],
+      countries: ['China','Japan'],
       list: [''],
     };
   },
