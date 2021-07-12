9c1593e3a6796bfbfdf9c112e6d146ab1f5f6e4e
Xi Lan <48905285+IAmParasite@users.noreply.github.com>
2021-07-12 23:41:21 +0800

    fix: 增加世界各国历史数据对比

--- src/views/GlobalAnalysis.vue
+++ src/views/GlobalAnalysis.vue
@@ -9,7 +9,7 @@
         ></el-page-header>
         <div v-if="country == 'World'">世界疫情数据</div>
       </div>
-      <el-tabs :tab-position="'left'" style="height: 600px; margin-top: 5px">
+      <el-tabs :tab-position="'left'" style="height: 700px; margin-top: 5px">
         <el-tab-pane class="tabPane" label="疫情地图">
           <div class="MapMain">
             <div class="Map">
