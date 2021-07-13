58a44924befcdb1cf152f50e38bd3a2c8d6b2b24
Never-Panic <954001442@qq.com>
2021-07-13 10:24:29 +0800

    fix: travel page layout

--- src/components/common/TravelCard.vue
+++ src/components/common/TravelCard.vue
@@ -1,7 +1,7 @@
 <template>
 	<div class="travelCardRoot">
 	
-		<div style="margin-left: 30px">
+		<div style="margin-left: 30px; display: flex; flex-direction: column; justify-content: center; align-items: center;">
 			<div class="travelCity" >
 				{{start}}
 			</div>
@@ -18,14 +18,14 @@
                 viewBox="0 0 1000 1000"><path fill="#666666" d="M796.014 412.647l-257.492-257.492c-20.11-20.11-52.709-20.11-72.819 0l-257.492 257.492c-20.11 20.11-20.11 52.709 0 72.819s52.709 20.11 72.819 0l169.585-169.585v493.664c0 28.453 23.046 51.499 51.499 51.499s51.499-23.046 51.499-51.499v-493.664l169.585 169.585c10.042 10.043 23.226 15.089 36.41 15.089s26.367-5.021 36.41-15.089c20.11-20.11 20.11-52.709 0-72.819z" /></svg>
 		</div>
 		
-		<div style="margin-right: 30px;">
+		<div style="margin-right: 30px; display: flex; flex-direction: column; justify-content: center; align-items: center;">
 			<div class="travelCity">
 				{{destination}}
 			</div>
 			{{arriveTime}}
 		</div>
 
-		<el-tag type="warning" size='big' effect="dark" style="font-size: 15px; margin-right: 30px;"><b>{{status}}</b></el-tag>
+		<el-tag v-if="status!=undefined" type="warning" size='big' effect="dark" style="font-size: 15px; margin-right: 30px;"><b>{{status}}</b></el-tag>
 
 	</div>
 </template>
